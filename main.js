97ae80b8f0294addba3f1695a6dc9069dfc4f5a5
mmeacham92 <meacham.michael@rocketmail.com>
2021-07-02 15:01:45 -0500

    added find method to check for logged in user; started renderUser function

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 let users = [
   {
     id: 1,
+    fullName: 'Roger Buck',
     username: "1111",
     password: "1111",
     transactions: [100, 200, -300, 200],
   },
   {
     id: 2,
-    username: "3412",
+    fullName: 'Tom Jones',
+    username: "2222",
     password: "2222",
     transactions: [200, -200, -300, 200],
   },
   {
     id: 3,
+    fullName: 'John Travolta',
     username: "3333",
     password: "3333",
+    transactions: [1000, -300, -300, 100],
+  },
+  {
+    id: 4,
+    fullName: 'Sally Fields',
+    username: "4444",
+    password: "4444",
     transactions: [1000, -300, -300, 100],
   },
 ];
@@ -28,8 +38,37 @@ loginButton.addEventListener("click", function (event) {
   let password = document.querySelector("#password").value;
   console.log(username, password);
 
-//   let currentUser = users.find(function (user) {
-//     return user.username === username;
-// })
-//     console.log(currentUser);
+  // see if the username even exists in our users array
+  // let currentUser = {};
+  // for (let index = 0; index < users.length; index += 1) {
+  //   let user = users[index];
+  //   if it does exists, see if the password matches
+  //   if (username === user.username && password === user.password) {
+  //     currentUser = user;
+  //   }
+  // }
+  // document.write(JSON.stringify(currentUser))
+  
+
+  // let currentUser = users.find(function (user) {
+  //   return username === user.username && password === user.password;
+  // });
+  let currentUser = users.find(user => username === user.username && password === user.password)
+
+  // if the password matches, render their data
+  renderUser(currentUser);
+
+  console.log(currentUser);
 });
+
+function renderUser(user) {
+  let form = document.querySelector(".login-form");
+  form.classList.toggle('hidden');
+  let userInfoDiv = document.querySelector('.user-info-div');
+  userInfoDiv.classList.toggle('hidden');
+
+  // populate each field in the user info div
+  let userSpan = document.querySelector('.user-name');
+  userSpan.innerText = user.fullName;
+
+}
